8b1904013f503295edb9043698a550003e66aeda
spark423 <seanpark8181@gmail.com>
2017-12-09 23:17:18 -0500

    Added else statement to handle for null response

--- routes/drawer.js
+++ routes/drawer.js
@@ -11,22 +11,32 @@ module.exports = function(passport) {
     Time.findOneAndUpdate({}, {$push: {follows: {createdAt: Date.now(), user:req.user._id}}}, function(err, time) {
       if (err) {
         throw err;
-      } else if (Array.isArray(req.body["subscribedBoards[]"])){
-        User.findOneAndUpdate({_id: req.user._id}, {$set: {subscribedBoards: req.body["subscribedBoards[]"]}},function(err,user) {
-          if (err) {
-            throw err;
-          } else {
-            res.json({success: true})
-          }
-        })        
+      } else if (req.body["subscribedBoards[]"]) {
+        if (Array.isArray(req.body["subscribedBoards[]"])){
+          User.findOneAndUpdate({_id: req.user._id}, {$set: {subscribedBoards: req.body["subscribedBoards[]"]}},function(err,user) {
+            if (err) {
+              throw err;
+            } else {
+              res.json({success: true})
+            }
+          })        
+        } else {
+          User.findOneAndUpdate({_id: req.user._id}, {$set: {subscribedBoards: [req.body["subscribedBoards[]"]]}},function(err,user) {
+            if (err) {
+              throw err;
+            } else {
+              res.json({success: true})
+            }
+          })    
+        }
       } else {
-        User.findOneAndUpdate({_id: req.user._id}, {$addToSet: {subscribedBoards: req.body["subscribedBoards[]"]}},function(err,user) {
+        User.findOneAndUpdate({_id: req.user._id}, {$set: {subscribedBoards: []}},function(err,user) {
           if (err) {
             throw err;
           } else {
             res.json({success: true})
           }
-        })    
+        }) 
       }
     }) 
   }); 
