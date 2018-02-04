1d47d35cd36206b66650d1442b30d97f3923c148
spark423 <seanpark8181@gmail.com>
2018-02-04 09:34:05 -0500

    Updated notification model and get method

--- models/notification.js
+++ models/notification.js
@@ -2,14 +2,15 @@ var mongoose = require('mongoose');
 
 var notificationSchema = new mongoose.Schema({
 	to: {type: mongoose.Schema.Types.ObjectId, refPath: 'User'},
-	type: {type: String},
 	createdAt: {type: Date, default: Date.now},
 	message: {type: String},
 	routeID: {
 		kind: String,
 		id: {type: mongoose.Schema.Types.ObjectId, refPath: 'source.kind'},
 		boardId: {type: mongoose.Schema.Types.ObjectId, refPath: 'Board'}
-	}
+	},
+	read: {type: Boolean, default: false},
+	readAt: {type: Date}
 });
 
 module.exports = mongoose.model('Notification', notificationSchema);
