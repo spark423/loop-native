08af3d7e43bc3a2d0f631f8c81e3c6c103cc7a85
spark423 <seanpark8181@gmail.com>
2018-02-16 19:38:27 -0500

    fixed board route

--- models/board.js
+++ models/board.js
@@ -3,10 +3,12 @@ var mongoose = require('mongoose');
 var boardSchema = new mongoose.Schema({  
   name: {type: String, required: true},
   description: {type: String},
-  create: {type: Boolean},
-  unsubscribable: {type: Boolean},
-  private: {type: Boolean},
+  create: {type: Boolean, default: true},
+  unsubscribable: {type: Boolean, default: false},
+  private: {type: Boolean, default: false},
   asset: {type: String},
+  archive: {type: Boolean, default: false},
+  active: {type: Boolean, default: true},  
   notifications: [{type: mongoose.Schema.Types.ObjectId, ref: 'Notification'}],
   contents: [{
     kind: String,
